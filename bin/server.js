bf21f7a4f6675bf742228ce50bb7ac24cc381e42
R0st <dotarost@mail.ru>
2021-11-12 06:59:45 +0200

    add swagger begining

--- bin/server.js
+++ bin/server.js
@@ -1,17 +1,25 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
+require("dotenv").config();
+// require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
-const app = require('../app')
+const app = require("../app");
 
-const { DB_HOST, PORT = 3030 } = process.env
+const { DB_HOST, PORT = 3030 } = process.env;
 
-mongoose.connect(DB_HOST, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  app.listen(PORT)
-  console.log('Database connection successful')
-}).catch(error => {
-  console.log(error.message)
-  process.exit(1)
-})
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT);
+    console.log(`Database connection successful on port ${PORT}`);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
