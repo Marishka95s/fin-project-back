a3a1e49f1e6b2eda1bcbaa497011901517f453f8
Marishka95s <marishka95@ukr.net>
2021-11-12 23:15:44 +0200

    creation branch auth2

--- schemas/index.js
+++ schemas/index.js
@@ -4,10 +4,9 @@ const { User } = require('./users')
 const { RefreshToken } = require('./refreshTokens')
 const { categories } = require('./transactions')
 
-
 module.exports = {
   Transaction,
   User,
-  RefreshToken
+  RefreshToken,
   categories,
 }
