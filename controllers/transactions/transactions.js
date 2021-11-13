77781735b37226e55adddbb87f6685c2aa678124
BondarenkoAlla <bondarenkoalla961@gmail.com>
2021-11-13 15:00:37 +0200

    8 not 10

--- controllers/transactions/transactions.js
+++ controllers/transactions/transactions.js
@@ -4,7 +4,7 @@ const { User } = require('../../schemas')
 const { categories } = require('../../schemas')
 
 const listTransactions = async (req, res, next) => {
-  const { page = 1, limit = 10 } = req.query
+  const { page = 1, limit = 8 } = req.query
   const skip = (page - 1) * limit
   const { _id } = req.user
   const transactions = await Transaction.find({ owner: _id }, '_id type category sum comment date month year balance owner ', { skip, limit: +limit }).populate('owner', 'email')
