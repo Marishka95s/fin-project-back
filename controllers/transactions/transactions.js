5d4be5670213e505e590bd0fe8f4e9bfdad55559
Kuzmeko Vladimir <wmeywise@gmail.com>
2021-11-15 07:06:01 +0200

    remove

--- controllers/transactions/transactions.js
+++ controllers/transactions/transactions.js
@@ -131,81 +131,14 @@ const getStatistics = async (req, res, next) => {
     })
 }
 
-const updateById = async (req, res, next) => {
-  const { transactionId } = req.params
-  const result = await Transaction.findByIdAndUpdate(transactionId, req.body)
-  if (!result) {
-    res.status(404).json({
-      status: 'error',
-      code: 404,
-      message: `Contact with id ${transactionId} not found`
-    })
-    return
-  }
-  res.json({
-    status: 'successfuly updated',
-    code: 202,
-    result
-  })
-}
-
-const removeById = async (req, res, next) => {
-  const { transactionId } = req.params
-  const transaction = await Transaction.findByIdAndRemove(transactionId)
-  if (!transaction) {
-    res.status(404).json({
-      status: 'error',
-      code: 404,
-      message: `Contact with id ${transactionId} not found`
-    })
-    return
-  }
-  res.json(
-    {
-      status: 'successfully deleted',
-      code: 204
-    })
-}
-
-const updateTransactionStatusById = async (req, res) => {
-  if (req.body.favorite === undefined) {
-    res.status(400).json({
-      status: 'error',
-      code: 404,
-      message: 'missing field favorite'
-    })
-    return
-  }
-  const { transactionId } = req.params
-  const result = await Transaction.findByIdAndUpdate(transactionId, req.body)
-  if (!result) {
-    res.status(404).json({
-      status: 'error',
-      code: 404,
-      message: `Contact with id ${transactionId} not found`
-    })
-    return
-  }
-  res.json({
-    status: 'successfuly updated',
-    code: 202
-  })
-}
 const getCategories = async (req, res) => {
-  res.json({
-    status: 'success',
-    code: 200,
-    data: categories
-  })
+  res.json({ data: categories })
 }
 
 module.exports = {
   listTransactions,
   getById,
   add,
-  updateById,
-  removeById,
-  updateTransactionStatusById,
   getCategories,
   getStatistics
 }
--- routes/api/transaction.js
+++ routes/api/transaction.js
@@ -16,10 +16,4 @@ router.get('/:transactionId', authenticate, controllerWrapper(ctrl.getById))
 
 router.post('/', authenticate, validator(joiSchema), controllerWrapper(ctrl.add))
 
-router.delete('/:transactionId', authenticate, controllerWrapper(ctrl.removeById))
-
-router.put('/:transactionId', authenticate, validator(joiSchema), controllerWrapper(ctrl.updateById))
-
-router.patch('/:transactionId/favorite', authenticate, controllerWrapper(ctrl.updateTransactionStatusById))
-
 module.exports = router
