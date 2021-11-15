439ab4aa53d98d180ea3a23fc0cc1209bea03f27
Kuzmeko Vladimir <wmeywise@gmail.com>
2021-11-15 08:06:12 +0200

    удалил закоментированные описания описание из раута авторизации

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,7 @@ const { users: ctrl } = require('../../controllers')
 const router = express.Router()
 
 router.get('/verify/:verificationToken', controllerWrapper(ctrl.verify))
+
 router.post('/verify/', controllerWrapper(ctrl.verifyAgain))
 
 router.patch('/avatars', authenticate, upload.single('avatar'), sizeChanger, controllerWrapper(ctrl.updateAvatar))
