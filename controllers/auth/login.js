1b3d841d0d3d11072d40c3ccf2b6e0473148e87b
Mariia Kontseva <73597030+Marishka95s@users.noreply.github.com>
2021-11-16 22:10:58 +0200

    token into 2h

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -25,7 +25,7 @@ const login = async (req, res) => {
     id: user._id
   }
 
-  const token = `Bearer ${jwt.sign(payload, SECRET_KEY, { expiresIn: '15m' })}`
+  const token = `Bearer ${jwt.sign(payload, SECRET_KEY, { expiresIn: '120m' })}`
   const refreshToken = `Bearer ${crypto.randomBytes(40).toString('hex')}`
   const refreshTokenConection = new RefreshToken({
     user: user._id,
